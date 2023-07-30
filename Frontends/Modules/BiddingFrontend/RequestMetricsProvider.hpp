6ac8c6598c10d4318c6ab6d4ac3b96744c93eaf7
Sergey Belyalov <akakist@gmail.com>
2023-07-30 11:38:04 +0000

    cfg fone

--- Frontends/Modules/BiddingFrontend/RequestMetricsProvider.hpp
+++ Frontends/Modules/BiddingFrontend/RequestMetricsProvider.hpp
@@ -1,3 +1,5 @@
+#ifdef KALL
+
 #ifndef BIDDINGFRONTEND_REQUESTMETRICSPROVIDER_HPP
 #define BIDDINGFRONTEND_REQUESTMETRICSPROVIDER_HPP
 
@@ -64,3 +66,4 @@ using RequestMetricsProvider_var = ReferenceCounting::SmartPtr<RequestMetricsPro
 } // namespace AdServer
 
 #endif //BIDDINGFRONTEND_REQUESTMETRICSPROVIDER_HPP
+#endif
