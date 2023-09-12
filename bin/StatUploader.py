8e37f952617f8dd2208620035504df1dc8ae5a98
AntonKulikov <anton.kulikov@mail.ru>
2023-09-12 23:36:25 +0000

    StatUploader: failure dir

--- bin/StatUploader.py
+++ bin/StatUploader.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 import os
+import subprocess
+import shutil
 from ServiceUtilsPy.Service import Service
 from ServiceUtilsPy.Context import Context
 
@@ -9,6 +11,9 @@ class Upload:
     def __init__(self, service, params, table_name):
         self.service = service
         self.__in_dir = params["in_dir"]
+        self.__failure_dir = params.get("failure_dir")
+        if self.__failure_dir is not None:
+            os.makedirs(self.__failure_dir, exist_ok=True)
         self.__table_name = table_name
 
     def process(self):
@@ -16,12 +21,15 @@ class Upload:
             for in_file in ctx.files.get_in_files():
                 self.service.print_(0, f"Processing {in_file}")
                 in_path = os.path.join(ctx.in_dir, in_file)
-                exit_status = os.system(
-                    f'cat "{in_path}" | clickhouse-client -h "{self.service.ch_host}" --query="INSERT INTO {self.__table_name} FORMAT CSV"')
-                if exit_status != 0:
-                    raise RuntimeError(f"Bad clickhouse-client exit status {exit_status}")
-                self.service.print_(0, f"Removing {in_file}")
-                os.remove(in_path)
+                cmd = f'cat "{in_path}" | clickhouse-client -h "{self.service.ch_host}" --query="INSERT INTO {self.__table_name} FORMAT CSV"'
+                with subprocess.Popen(['sh', '-c', cmd], stdout=subprocess.PIPE, stderr=subprocess.PIPE) as proc:
+                    stdout, stderr = proc.communicate()
+                    if proc.returncode == 0 or self.__failure_dir is None:
+                        self.service.print_(0, f"Removing {in_file}")
+                        os.remove(in_path)
+                    else:
+                        self.service.print_(0, f"Failure in {in_file}:\n{stderr.decode()}")
+                        shutil.move(in_path, self.__failure_dir)
 
 
 class RequestStatsHourlyExtStatUpload(Upload):
@@ -39,6 +47,7 @@ class Application(Service):
         super().__init__()
         self.args_parser.add_argument("--ch-host", help="Clickhouse hostname.")
         self.args_parser.add_argument("--upload-type", help="Upload type (for single upload).")
+        self.args_parser.add_argument("--failure-dir", help="Directory that stores failure files (for single upload).")
         self.__uploads = []
 
     def on_start(self):
