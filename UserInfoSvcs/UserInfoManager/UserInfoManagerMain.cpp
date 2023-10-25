eff4db099a3f5dd22dfb2d2626ed9846f8aeb17d
akakist <akakist@gmail.com>
2023-10-25 20:12:56 +0300

    editing

--- UserInfoSvcs/UserInfoManager/UserInfoManagerMain.cpp
+++ UserInfoSvcs/UserInfoManager/UserInfoManagerMain.cpp
@@ -8,6 +8,7 @@
 
 #include "UserInfoManagerMain.hpp"
 #include "UserInfoManagerStat.hpp"
+#include "UServerUtils/MetricsHTTPProvider.hpp"
 
 namespace
 {
@@ -20,7 +21,8 @@ namespace
 
 UserInfoManagerApp_::UserInfoManagerApp_() /*throw(eh::Exception)*/
   : AdServer::Commons::ProcessControlVarsLoggerImpl(
-      "UserInfoManagerApp_", ASPECT)
+      "UserInfoManagerApp_", ASPECT),
+    composite_metrics_provider_(new Generics::CompositeMetricsProvider)
 {}
 
 void
@@ -223,6 +225,17 @@ UserInfoManagerApp_::main(int& argc, char** argv)
       PROCESS_CONTROL_OBJ_KEY, this);
 
     shutdowner_ = corba_server_adapter_->shutdowner();
+
+    if(config()->Monitoring().present())
+    {
+      UServerUtils::MetricsHTTPProvider_var metrics_http_provider =
+        new UServerUtils::MetricsHTTPProvider(
+          composite_metrics_provider_,
+          config()->Monitoring()->port(),
+          "/metrics");
+
+      add_child_object(metrics_http_provider);
+    }
 
     activate_object();
 
