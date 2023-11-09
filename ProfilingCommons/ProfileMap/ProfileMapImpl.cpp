f9edc82522ee4d63f5acdcf5cb02457cc7c27342
Artem <bogdanovartemn@gmail.com>
2023-11-09 17:37:56 +0000

    Adding io_uring rocksdb to profile_map

---
+++ ProfilingCommons/ProfileMap/ProfileMapImpl.cpp
@@ -0,0 +1,183 @@
+// STD
+#include <thread>
+
+// THIS
+#include "ProfileMapImpl.hpp"
+
+namespace AdServer::ProfilingCommons
+{
+
+namespace Internal
+{
+
+ProfileMapImpl::ProfileMapImpl(
+  Logger* logger,
+  const std::uint64_t memtable_memory_budget_mb,
+  const std::uint64_t block_сache_size_mb,
+  const std::string& db_path,
+  std::optional<std::int32_t> ttl,
+  std::optional<std::string> column_family_name)
+  : logger_(ReferenceCounting::add_ref(logger)),
+    db_path_(db_path)
+{
+  std::size_t number_threads = std::thread::hardware_concurrency();
+  if (number_threads == 0)
+  {
+    number_threads = 16;
+  }
+
+  rocksdb::DBOptions db_options;
+  db_options.IncreaseParallelism(number_threads);
+  db_options.create_if_missing = true;
+
+  rocksdb::ColumnFamilyOptions column_family_options;
+  column_family_options.OptimizeLevelStyleCompaction(
+    memtable_memory_budget_mb);
+  column_family_options.OptimizeForPointLookup(
+    block_сache_size_mb);
+  column_family_options.target_file_size_multiplier = 2;
+
+  if (!column_family_name.has_value())
+  {
+    column_family_name = rocksdb::kDefaultColumnFamilyName;
+  }
+
+  std::optional<std::vector <std::int32_t>> ttls;
+  if (ttl.has_value())
+  {
+    ttls = std::vector<std::int32_t>{*ttl};
+  }
+
+  std::vector<rocksdb::ColumnFamilyDescriptor> descriptors{
+    {*column_family_name, column_family_options}};
+
+  data_base_ = std::make_shared<DataBase>(
+    logger_.in(),
+    db_path,
+    db_options,
+    descriptors,
+    true,
+    ttls);
+
+  column_family_handle_ = &data_base_->column_family(
+    *column_family_name);
+
+  ManagerPoolConfig config;
+  config.event_queue_max_size = 1000000;
+  config.io_uring_flags = IORING_SETUP_ATTACH_WQ;
+  config.io_uring_size = 6400;
+  config.number_io_urings = number_threads;
+  db_manager_pool_ = std::make_unique<DataBaseManagerPool>(
+    config,
+    logger_.in());
+}
+
+ProfileMapImpl::ConstSmartMemBuf_var
+ProfileMapImpl::ProfileMapImpl::get_profile(
+  const std::string_view key)
+{
+  std::string value;
+  const auto status = db_manager_pool_->get(
+    data_base_,
+    *column_family_handle_,
+    read_options_,
+    key,
+    value);
+
+  if(status.IsNotFound())
+  {
+    return {};
+  }
+
+  if (!status.ok())
+  {
+    Stream::Error stream;
+    stream << FNS
+           << ": can't read record from DB: "
+           << db_path_
+           << " with key="
+           << key;
+    throw Exception(stream);
+  }
+
+  return ConstSmartMemBuf_var(
+    new Generics::ConstSmartMemBuf(
+      value.data(),
+      value.size()));
+}
+
+bool ProfileMapImpl::check_profile(const std::string_view key) const
+{
+  std::string value;
+  const auto status = db_manager_pool_->get(
+    data_base_,
+    *column_family_handle_,
+    read_options_,
+    key,
+    value);
+
+  if (!status.IsNotFound())
+  {
+    return false;
+  }
+
+  if (!status.ok())
+  {
+    Stream::Error stream;
+    stream << FNS
+           << ": can't read record from DB: "
+           << db_path_
+           << " with key="
+           << key;
+    throw Exception(stream);
+  }
+
+  return true;
+}
+
+void ProfileMapImpl::save_profile(
+  const std::string_view key,
+  const Generics::ConstSmartMemBuf* profile)
+{
+  const std::string_view value(
+    static_cast<const char*>(profile->membuf().data()),
+    profile->membuf().size());
+  const auto status = db_manager_pool_->put(
+    data_base_,
+    *column_family_handle_,
+    write_options_,
+    key,
+    value);
+  if (!status.ok())
+  {
+    Stream::Error stream;
+    stream << FNS
+           << ": can't save record to DB '"
+           << db_path_
+           << "': "
+           << status.ToString();
+    throw Exception(stream);
+  }
+}
+
+bool ProfileMapImpl::remove_profile(
+  const std::string_view key)
+{
+  const auto status = db_manager_pool_->erase(
+    data_base_,
+    *column_family_handle_,
+    write_options_,
+    key);
+ if (status.ok() || status.IsNotFound())
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+} // namespace Internal
+
+} // namespace AdServer::ProfilingCommons
---
+++ ProfilingCommons/ProfileMap/ProfileMapImpl.hpp
@@ -0,0 +1,166 @@
+#ifndef PROFILEMAPIMPL_HPP_
+#define PROFILEMAPIMPL_HPP_
+
+// STD
+#include <optional>
+
+// UNIXCOMMONS
+#include <Generics/Uncopyable.hpp>
+#include <ReferenceCounting/AtomicImpl.hpp>
+#include <ReferenceCounting/SmartPtr.hpp>
+#include <UServerUtils/Grpc/RocksDB/DataBase.hpp>
+#include <UServerUtils/Grpc/RocksDB/DataBaseManagerPool.hpp>
+
+// THIS
+#include "ProfileMap.hpp"
+
+namespace AdServer::ProfilingCommons
+{
+
+namespace Internal
+{
+
+class ProfileMapImpl final: Generics::Uncopyable
+{
+public:
+  using Logger = Logging::Logger;
+  using Logger_var = Logging::Logger_var;
+
+  DECLARE_EXCEPTION(Exception, eh::DescriptiveException);
+
+private:
+  using DataBase = UServerUtils::Grpc::RocksDB::DataBase;
+  using DataBasePtr = std::shared_ptr<DataBase>;
+  using DataBaseManagerPool = UServerUtils::Grpc::RocksDB::DataBaseManagerPool;
+  using DataBaseManagerPoolPtr = std::unique_ptr<DataBaseManagerPool>;
+  using ManagerPoolConfig = UServerUtils::Grpc::RocksDB::Config;
+  using ColumnFamilyHandle = DataBaseManagerPool::ColumnFamilyHandle;
+  using ReadOptions = rocksdb::ReadOptions;
+  using WriteOptions = rocksdb::WriteOptions;
+  using ConstSmartMemBuf_var = Generics::ConstSmartMemBuf_var;
+  using Time = Generics::Time;
+
+public:
+  explicit ProfileMapImpl(
+    Logger* logger,
+    const std::uint64_t memtable_memory_budget_mb,
+    const std::uint64_t block_сache_size_mb,
+    const std::string& db_path,
+    std::optional<std::int32_t> ttl = {},
+    std::optional<std::string> column_family_name = {});
+
+  ~ProfileMapImpl() = default;
+
+  bool check_profile(const std::string_view key) const;
+
+  ConstSmartMemBuf_var get_profile(
+    const std::string_view key);
+
+  void save_profile(
+    const std::string_view key,
+    const Generics::ConstSmartMemBuf* profile);
+
+  bool remove_profile(
+    const std::string_view key);
+
+private:
+  Logger_var logger_;
+
+  const std::string db_path_;
+
+  DataBasePtr data_base_;
+
+  DataBaseManagerPoolPtr db_manager_pool_;
+
+  ColumnFamilyHandle* column_family_handle_ = nullptr;
+
+  ReadOptions read_options_;
+
+  WriteOptions write_options_;
+};
+
+} // namespace Internal
+
+struct DefaultKeyAdapter
+{
+  template<typename Key>
+  std::string operator()(const Key& key)
+  {
+    return key;
+  }
+};
+
+template<class Key, class KeyAdapter = DefaultKeyAdapter>
+class ProfileMapImpl final:
+  public ProfileMap<Key>,
+  public ReferenceCounting::AtomicImpl
+{
+public:
+  using Logger = Logging::Logger;
+  using Logger_var = Logging::Logger_var;
+  using ConstSmartMemBuf_var = Generics::ConstSmartMemBuf_var;
+
+public:
+  explicit ProfileMapImpl(
+    Logger* logger,
+    const std::uint64_t memtable_memory_budget_mb,
+    const std::uint64_t block_сache_size_mb,
+    const std::string& db_path,
+    std::optional<std::int32_t> ttl = {},
+    std::optional<std::string> column_family_name = {})
+    : impl_(
+        memtable_memory_budget_mb,
+        block_сache_size_mb,
+        db_path,
+        ttl,
+        column_family_name)
+  {
+  }
+
+  bool check_profile(const Key& key) const override
+  {
+    return impl_.check_profile(key_adapter_(key));
+  }
+
+  ConstSmartMemBuf_var get_profile(
+    const Key& key,
+    Generics::Time* /*last_access_time*/) override
+  {
+    return impl_.get_profile(key_adapter_(key));
+  }
+
+  void save_profile(
+    const Key& key,
+    const Generics::ConstSmartMemBuf* profile,
+    const Generics::Time& /*now*/,
+    OperationPriority /*op_priority*/) override
+  {
+    impl_.save_profile(key_adapter_(key), profile);
+  }
+
+  bool remove_profile(
+    const Key& key,
+    OperationPriority /*op_priority*/) override
+  {
+    return impl_.remove_profile(key_adapter_(key));
+  }
+
+  unsigned long size() const noexcept override
+  {
+    return 1;
+  }
+
+  unsigned long area_size() const noexcept override
+  {
+    return 1;
+  }
+
+private:
+  KeyAdapter key_adapter_;
+
+  Internal::ProfileMapImpl impl_;
+};
+
+} // namespace AdServer::ProfilingCommons
+
+#endif //PROFILEMAPIMPL_HPP_
