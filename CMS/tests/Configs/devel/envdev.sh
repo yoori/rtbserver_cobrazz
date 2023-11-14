09e521af92894c65cf12917669490f2062f249e6
Sergey Belyalov <akakist@gmail.com>
2023-11-14 09:58:44 +0000

    done xml

--- CMS/tests/Configs/devel/envdev.sh
+++ CMS/tests/Configs/devel/envdev.sh
@@ -6,5 +6,5 @@ export unixcommons_root=$HOME/projects/unixcommons/trunk
 export PATH=$PATH:$server_root/bin
 export PATH=$PATH:$server_root/ConfigSys
 export PATH=$PATH:$server_root/build/bin
-export VERSION='3.5.6.274'
+export VERSION='4.0.0.19'
 export LOG_LEVEL=7
