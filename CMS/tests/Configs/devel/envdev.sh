ce355177c79e39bf546b94a85cb493f436d3ae50
Yuri Kuznecov <yuri.kuznecov@gmail.com>
2024-03-18 08:15:25 +0000

    Developer config templates fixes

--- CMS/tests/Configs/devel/envdev.sh
+++ CMS/tests/Configs/devel/envdev.sh
@@ -1,10 +1,16 @@
-# source rtbserver directory
-#export server_root=$HOME/projects/oix/server/trunk
-export server_root=$HOME/yoori/rtbserver_cobrazz
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 
-export config_root=$HOME/yoori/run/config
-export workspace_root=$HOME/yoori/run/workspace
-export unixcommons_root=$HOME/yoori/unixcommons_cobrazz
+export user=$(id -u -n)
+export usergroup=$(id -g -n)
+
+# source root's
+export unixcommons_root=$HOME/projects/unixcommons_cobrazz
+export server_root=$HOME/projects/rtbserver_cobrazz
+
+# cluster directories
+export config_root=$HOME/run/etc/
+export workspace_root=$HOME/run/var/
 
 export PATH=$PATH:$server_root/bin
 export PATH=$PATH:$server_root/ConfigSys
