ff5a3430816ed0b242d48485d7f752213346c46a
Yuri Kuznecov <yuri.kuznecov@gmail.com>
2025-01-21 09:08:59 +0000

    RImpression, RClick uploader: added batch uploading for avoid clickhouse much portions errors

--- LogProcessing/ClickhouseUploader/RClickClickhouseAdapter.py
+++ LogProcessing/ClickhouseUploader/RClickClickhouseAdapter.py
@@ -6,42 +6,45 @@ import argparse
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='Default yolo11 model training script.')
-  parser.add_argument('filename')
+  parser.add_argument('filename', nargs='+')
   args = parser.parse_args()
 
-  with open(args.filename, 'r') as infile:
-    field_filling = [  # < Order of fields in clickhouse table RImpression.
-      ('request_id', 1),
-      ('timestamp', None),
-      ('device', None),
-      ('ip', None),
-      ('uid', None),
-      ('url', None),
-      ('publisher_id', None),
-      ('tag_id', None),
-      ('etag', None),
-      ('campaign_id', None),
-      ('ccg_id', None),
-      ('ccid', None),
-      ('geo_ch', None),
-      ('user_ch',  None),
-      ('imp_ch', None),
-      ('bid_price', None),
-      ('bid_floor', None),
-      ('alg_id', None),
-      ('size_id', None),
-      ('colo_id', None),
-      ('predicted_ctr', None),
-      ('campaign_freq', None),
-      ('cr_alg_id', None),
-      ('predicted_cr', None),
-      ('win_price', None),
-      ('viewability', None),
-      ('click_timestamp', 0),
-    ]
-    writer = csv.writer(sys.stdout)
-    writer.writerow([field[0] for field in field_filling])
-    it = iter(csv.reader(infile))
-    next(it)  # skip header - it contains problem
-    for row in it:
-      writer.writerow([(row[field_index] if field_index is not None else '') for _, field_index in field_filling])
+  field_filling = [  # < Order of fields in clickhouse table RImpression.
+    ('request_id', 1),
+    ('timestamp', None),
+    ('device', None),
+    ('ip', None),
+    ('uid', None),
+    ('url', None),
+    ('publisher_id', None),
+    ('tag_id', None),
+    ('etag', None),
+    ('campaign_id', None),
+    ('ccg_id', None),
+    ('ccid', None),
+    ('geo_ch', None),
+    ('user_ch',  None),
+    ('imp_ch', None),
+    ('bid_price', None),
+    ('bid_floor', None),
+    ('alg_id', None),
+    ('size_id', None),
+    ('colo_id', None),
+    ('predicted_ctr', None),
+    ('campaign_freq', None),
+    ('cr_alg_id', None),
+    ('predicted_cr', None),
+    ('win_price', None),
+    ('viewability', None),
+    ('click_timestamp', 0),
+  ]
+
+  writer = csv.writer(sys.stdout)
+  writer.writerow([field[0] for field in field_filling])
+
+  for read_file in args.filename:
+    with open(read_file, 'r') as infile:
+      it = iter(csv.reader(infile))
+      next(it)  # skip header - it contains problem
+      for row in it:
+        writer.writerow([(row[field_index] if field_index is not None else '') for _, field_index in field_filling])
--- LogProcessing/ClickhouseUploader/RImpressionStatUploader.py
+++ LogProcessing/ClickhouseUploader/RImpressionStatUploader.py
@@ -8,6 +8,7 @@ import time
 import json
 import argparse
 import typing
+import shutil
 import jinja2
 
 
@@ -16,12 +17,14 @@ class Config :
   pid_file: str = None
   check_roots: typing.List[str] = []
   error_root: str = None
+  batch: int = 1000
 
   def init_json(self, config_json) :
     self.pid_file = config_json.get('pid_file', None)
     self.clickhouse_conn = config_json.get('clickhouse_conn', '')
     self.check_roots = config_json.get('check_roots', [])
     self.error_root = config_json.get('error_root', None)
+    self.batch = config_json.get('batch', 1000)
 
 
 class SignalInterruptHandler(object):
@@ -84,23 +87,27 @@ class RImpressionUploader(object) :
   def __init__(self, config, logger = None) :
     self.clickhouse_conn = config.clickhouse_conn
     self.command_line_templ = jinja2.Template(
-      "RImpressionClickhouseAdapter.py {{process_file}} | clickhouse-client {{clickhouse_conn}} " +
+      "RImpressionClickhouseAdapter.py {{ process_files|join(' ') }} | clickhouse-client {{clickhouse_conn}} " +
       '--query="INSERT INTO RImpression FORMAT CSV"')
     self.logger = logger
 
-  def process(self, process_file) :
+  def process(self, process_files) :
     # init sql for upload
-    command_line = self.command_line_templ.render({'clickhouse_conn' : self.clickhouse_conn, 'process_file' : process_file})
+    command_line = self.command_line_templ.render({
+      'clickhouse_conn' : self.clickhouse_conn, 'process_files' : process_files
+    })
     try :
-      self.logger.debug("To upload '" + process_file + "'")
+      self.logger.debug("To upload " + " ".join(process_files))
       ret_code = os.system(command_line)
-      self.logger.debug("From upload '" + process_file + "': " + str(ret_code))
+      self.logger.debug("From upload " + " ".join(process_files) + "': " + str(ret_code))
       if ret_code == 0 :
-        os.unlink(process_file)
+        for process_file in process_files:
+          os.unlink(process_file)
       else :
-        raise Exception("Error on upload '" + process_file + "': command_line = '" + command_line + "'")
+        raise Exception("Error on upload " + " ".join(process_files) + ": command_line = '" + command_line + "'")
     except Exception as e :
-      self.logger.exception("Exception on upload '" + process_file + "': " + str(e) + ", command_line = '" + command_line + "'")
+      self.logger.exception("Exception on upload " + " ".join(process_files) + ": " +
+        str(e) + ", command_line = '" + command_line + "'")
       raise
 
 
@@ -119,39 +126,69 @@ class RClickUploader(object) :
       '--query="INSERT INTO RImpression FORMAT CSV"')
     self.logger = logger
 
-  def process(self, process_file) :
+  def process(self, process_files) :
     # init sql for upload
-    command_line = self.command_line_templ.render({'clickhouse_conn' : self.clickhouse_conn, 'process_file' : process_file})
+    command_line = self.command_line_templ.render({
+      'clickhouse_conn' : self.clickhouse_conn,
+      'process_files' : process_files})
     try :
-      self.logger.debug("To upload '" + process_file + "'")
+      self.logger.debug("To upload " + " ".join(process_files))
       ret_code = os.system(command_line)
-      self.logger.debug("From upload '" + process_file + "': " + str(ret_code))
+      self.logger.debug("From upload " + " ".join(process_files) + ": " + str(ret_code))
       if ret_code == 0 :
-        os.unlink(process_file)
+        for process_file in process_files:
+          os.unlink(process_file)
       else :
-        self.logger.error("Error on upload '" + process_file + "': command_line = '" + command_line + "'")
+        self.logger.error("Error on upload " + " ".join(process_files) + ": command_line = '" + command_line + "'")
     except Exception as e :
-      self.logger.error("Exception on upload '" + process_file + "': " + str(e) + ", command_line = '" + command_line + "'")
+      self.logger.error("Exception on upload " + " ".join(process_files) + "': " + str(e) +
+        ", command_line = '" + command_line + "'")
 
 
-def check_stat_files(interrupter, config = None, logger = None, processors = None) :
+def check_stat_files(
+    interrupter,
+    config = None,
+    logger = None,
+    processors = None,
+) :
   for check_root in config.check_roots :
     logger.debug("Check root '" + check_root + "'")
     check_files = [ x for x in os.listdir(check_root) ]
+
+    processing_groups: typing.Dict[str, typing.List] = {}
+
     for check_file in check_files :
       logger.debug("Check file '" + check_file + "'")
       check_file_parts = check_file.replace('_', '.').split('.')
       if len(check_file_parts) > 0 :
         prefix = check_file_parts[0]
         if prefix in processors :
-          processor = processors[prefix]
           full_file = os.path.join(check_root, check_file)
-          try :
-            processor.process(full_file)
-          except Exception as e :
-            logger.exception("error on upload '" + check_file + "': " + str(e))
-            if config.error_root:
-              os.rename(full_file, config.error_root)
+          if prefix not in processing_groups:
+            processing_groups[prefix] = []
+          processing_groups[prefix].append(full_file)
+
+          if len(processing_groups[prefix]) >= config.batch:
+            process_files = processing_groups[prefix]
+            processor = processors[prefix]
+            try :
+              processor.process(process_files)
+            except Exception as e :
+              logger.exception("error on upload " + " ".join(process_files) + ": " + str(e))
+              if config.error_root:
+                for full_file in process_files:
+                  shutil.move(full_file, config.error_root)
+            processing_groups[prefix] = []
+
+    for prefix, process_files in processing_groups.items():
+      processor = processors[prefix]
+      try :
+        processor.process(process_files)
+      except Exception as e :
+        logger.exception("error on upload " + " ".join(process_files) + ": " + str(e))
+        if config.error_root:
+          for full_file in process_files:
+            shutil.move(full_file, config.error_root)
 
 
 def main() :
