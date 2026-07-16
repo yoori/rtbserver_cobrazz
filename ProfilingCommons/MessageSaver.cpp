cd3b3000921f1ceadb59f024c4895f67f737b11b
Yuri <yuri.kuznecov@gmail.com>
2026-07-16 16:25:07 +0000

    MessageSaver: make writing async (in backgroud)

--- ProfilingCommons/MessageSaver.cpp
+++ ProfilingCommons/MessageSaver.cpp
@@ -1,9 +1,13 @@
 #include "MessageSaver.hpp"
 
+#include <algorithm>
+
 #include <Logger/ActiveObjectCallback.hpp>
 
 #include <Commons/DelegateTaskGoal.hpp>
 #include <LogCommons/LogCommons.hpp>
+
+#include <utility>
 
 namespace Aspect
 {
@@ -14,23 +18,87 @@ namespace AdServer
 {
 namespace ProfilingCommons
 {
+  namespace
+  {
+    unsigned long normalize_write_workers(
+      unsigned long chunks_count,
+      unsigned long threads_count) noexcept
+    {
+      if(chunks_count == 0)
+      {
+        return 1;
+      }
+
+      return std::min(chunks_count, std::max(1UL, threads_count));
+    }
+  }
+
+  class MessageSaver::WriteWorkers final: public Generics::RefCountableSimpleActiveObject
+  {
+  public:
+    explicit WriteWorkers(MessageSaver* owner) noexcept
+      : owner_(owner)
+    {}
+
+  protected:
+    ~WriteWorkers() noexcept override = default;
+
+    void
+    activate_object_() override
+    {
+      try
+      {
+        owner_->start_write_workers_();
+      }
+      catch(...)
+      {
+        state_ = AS_DEACTIVATING;
+        owner_->notify_write_workers_();
+        owner_->wait_write_workers_();
+        throw;
+      }
+    }
+
+    void
+    deactivate_object_() override
+    {
+      owner_->notify_write_workers_();
+    }
+
+    void
+    wait_object_() override
+    {
+      owner_->wait_write_workers_();
+    }
+
+  private:
+    MessageSaver* const owner_;
+  };
+
+  MessageSaver::QueuedOperation::QueuedOperation(
+    unsigned long chunk_i_val,
+    std::uint32_t op_index_val,
+    Generics::MemBuf&& membuf_val) noexcept
+    : chunk_i(chunk_i_val),
+      op_index(op_index_val),
+      membuf(std::move(membuf_val))
+  {}
+
   MessageSaver::FileHolderGuard::FileHolderGuard(
     FileHolder* file_holder)
-    : guard_(file_holder->lock),
-      file_holder_(ReferenceCounting::add_ref(file_holder))
+    : file_holder_(ReferenceCounting::add_ref(file_holder)),
+      guard_(file_holder_->lock)
   {}
 
   void
-  MessageSaver::FileHolderGuard::write(
-    const void* buf, unsigned long size)
+  MessageSaver::FileHolderGuard::write(const void* buf, unsigned long size)
     /*throw(eh::Exception)*/
   {
     file_holder_->file_writer->write(buf, size);
   }
 
   void
-  MessageSaver::FileHolderGuard::write(
-    const Generics::MemBuf& membuf)
+  MessageSaver::FileHolderGuard::write(const Generics::MemBuf& membuf)
     /*throw(eh::Exception)*/
   {
     uint32_t sz = membuf.size();
@@ -44,29 +112,48 @@ namespace ProfilingCommons
     const char* output_dir,
     const char* output_file_prefix,
     unsigned long chunks_count,
-    const Generics::Time& flush_period)
+    const Generics::Time& flush_period,
+    unsigned long threads_count)
     : logger_(ReferenceCounting::add_ref(logger)),
       output_dir_(output_dir),
       output_file_prefix_(output_file_prefix),
       flush_period_(flush_period),
+      write_workers_count_(normalize_write_workers(chunks_count, threads_count)),
       callback_(new Logging::ActiveObjectCallbackImpl(
         logger,
         "ActiveObject",
         Aspect::MESSAGE_SAVER,
         "ADS-IMPL-?")),
+      write_workers_active_object_(new WriteWorkers(this)),
       task_runner_(new Generics::TaskRunner(callback_, 1)),
       scheduler_(new Generics::Planner(callback_))
   {
     static const char* FUN = "MessageSaver::MessageSaver()";
 
+    if (chunks_count == 0)
+    {
+      Stream::Error ostr;
+      ostr << FUN << ": chunks_count is zero";
+      throw Exception(ostr);
+    }
+
+    worker_conds_.reserve(write_workers_count_);
+    for(unsigned long i = 0; i < write_workers_count_; ++i)
+    {
+      worker_conds_.emplace_back(std::make_shared<QueueCond>());
+    }
+
     files_.reserve(chunks_count);
     for(unsigned long i = 0; i < chunks_count; ++i)
     {
-      files_.push_back(new FileHolder());
+      FileHolder* file_holder = new FileHolder();
+      file_holder->queue_cond = worker_conds_[i % write_workers_count_];
+      files_.push_back(file_holder);
     }
 
     try
     {
+      add_child_object(write_workers_active_object_);
       add_child_object(task_runner_);
       add_child_object(scheduler_);
     }
@@ -97,6 +184,7 @@ namespace ProfilingCommons
   {
     try
     {
+      wait_pending_operations_();
       flush();
     }
     catch(const eh::Exception&)
@@ -104,11 +192,10 @@ namespace ProfilingCommons
   }
 
   void
-  MessageSaver::write(
-    std::size_t distrib_hash,
-    const Generics::MemBuf& membuf)
+  MessageSaver::write(std::size_t distrib_hash, const Generics::MemBuf& membuf)
     /*throw(eh::Exception)*/
   {
+    check_background_error_();
     get_file_holder_(distrib_hash)->write(membuf);
   }
 
@@ -116,25 +203,60 @@ namespace ProfilingCommons
   MessageSaver::write_operation(
     std::size_t distrib_hash,
     unsigned long op_index,
-    const Generics::MemBuf& membuf)
+    Generics::MemBuf&& membuf)
     /*throw(eh::Exception)*/
   {
-    const uint32_t op_index_val = op_index;
+    check_background_error_();
 
-    FileHolderGuard_var file_holder_guard = get_file_holder_(distrib_hash);
-    file_holder_guard->write(&op_index_val, sizeof(op_index_val));
-    file_holder_guard->write(membuf);
+    const unsigned long chunk_i = distrib_hash % files_.size();
+    const uint32_t op_index_val = op_index;
+    bool notify_worker = false;
+    QueueCond* queue_cond = nullptr;
+
+    pending_operations_.fetch_add(1, std::memory_order_release);
+    try
+    {
+      FileHolder& file_holder = *files_[chunk_i];
+      std::lock_guard<std::mutex> guard(file_holder.queue_lock);
+      queue_cond = file_holder.queue_cond.get();
+      file_holder.queue.emplace_back(chunk_i, op_index_val, std::move(membuf));
+      notify_worker = queue_cond->queued_operations.fetch_add(
+        1,
+        std::memory_order_acq_rel) == 0;
+    }
+    catch(...)
+    {
+      if (pending_operations_.fetch_sub(1, std::memory_order_acq_rel) == 1)
+      {
+        std::lock_guard<std::mutex> lock(pending_cond_lock_);
+        pending_cond_.notify_all();
+      }
+      throw;
+    }
+
+    if (notify_worker)
+    {
+      std::lock_guard<std::mutex> lock(queue_cond->mutex);
+      queue_cond->cond.notify_one();
+    }
   }
 
   MessageSaver::FileHolderGuard_var
   MessageSaver::get_file_holder_(std::size_t hash)
     /*throw(eh::Exception)*/
   {
-    const unsigned long chunk_i = hash % files_.size();
-      // % files_.size() ~ % distrib_count
+    wait_pending_operations_();
+    check_background_error_();
+    return get_file_holder_i_(hash % files_.size());
+  }
+
+  MessageSaver::FileHolderGuard_var
+  MessageSaver::get_file_holder_i_(unsigned long chunk_i)
+    /*throw(eh::Exception)*/
+  {
     FileHolderGuard_var ret(new FileHolderGuard(files_[chunk_i]));
     FileHolder& file_holder = *files_[chunk_i];
-    if(file_holder.tmp_file_name.empty())
+    if (file_holder.tmp_file_name.empty())
     {
       init_file_holder_i_(file_holder, chunk_i);
     }
@@ -142,12 +264,12 @@ namespace ProfilingCommons
   }
 
   void
-  MessageSaver::flush(
-    FileNameList* dumped_files)
+  MessageSaver::flush(FileNameList* dumped_files)
     /*throw(eh::Exception)*/
   {
-    for(FileHolderArray::iterator it = files_.begin();
-        it != files_.end(); ++it)
+    wait_pending_operations_();
+    check_background_error_();
+    for(FileHolderArray::iterator it = files_.begin(); it != files_.end(); ++it)
     {
       dump_file_holder_(**it, dumped_files);
     }
@@ -166,18 +288,18 @@ namespace ProfilingCommons
     std::unique_ptr<AdServer::ProfilingCommons::FileWriter> file_writer;
 
     {
-      SyncPolicy::WriteGuard lock(file_holder.lock);
+      std::lock_guard<std::mutex> lock(file_holder.lock);
       tmp_file_name.swap(file_holder.tmp_file_name);
       file_name.swap(file_holder.file_name);
       file_writer.swap(file_holder.file_writer);
     }
 
-    if(!tmp_file_name.empty())
+    if (!tmp_file_name.empty())
     {
       file_writer->close();
       file_writer.reset(0);
 
-      if(::rename(tmp_file_name.c_str(), file_name.c_str()) < 0)
+      if (::rename(tmp_file_name.c_str(), file_name.c_str()) < 0)
       {
         Stream::Error ostr;
         ostr << FUN << ": can't rename '" << tmp_file_name <<
@@ -185,7 +307,7 @@ namespace ProfilingCommons
         eh::throw_errno_exception<Exception>(ostr.str());
       }
 
-      if(dumped_files)
+      if (dumped_files)
       {
         dumped_files->push_back(file_name);
       }
@@ -193,9 +315,7 @@ namespace ProfilingCommons
   }
 
   void
-  MessageSaver::init_file_holder_i_(
-    FileHolder& file_holder,
-    unsigned long chunk_i)
+  MessageSaver::init_file_holder_i_(FileHolder& file_holder, unsigned long chunk_i)
     /*throw(eh::Exception)*/
   {
     LogProcessing::LogFileNameInfo file_name_info(output_file_prefix_);
@@ -212,8 +332,7 @@ namespace ProfilingCommons
   }
 
   void
-  MessageSaver::flush_logs_()
-    noexcept
+  MessageSaver::flush_logs_() noexcept
   {
     static const char* FUN = "MessageSaver::flush_logs_()";
 
@@ -246,6 +365,192 @@ namespace ProfilingCommons
         "ADS-IMPL-?") << FUN <<
         ": Can't schedule next flush task: " << ex.what();
     }
+  }
+
+  void
+  MessageSaver::start_write_workers_()
+  {
+    write_workers_.reserve(write_workers_count_);
+    for(unsigned long i = 0; i < write_workers_count_; ++i)
+    {
+      write_workers_.emplace_back(
+        [this, i]()
+        {
+          write_worker_loop_(i);
+        });
+    }
+  }
+
+  void
+  MessageSaver::notify_write_workers_()
+  {
+    for(const auto& queue_cond : worker_conds_)
+    {
+      std::lock_guard<std::mutex> lock(queue_cond->mutex);
+      queue_cond->cond.notify_all();
+    }
+  }
+
+  void
+  MessageSaver::wait_write_workers_()
+  {
+    for (auto& worker : write_workers_)
+    {
+      if (worker.joinable())
+      {
+        worker.join();
+      }
+    }
+    write_workers_.clear();
+  }
+
+  void
+  MessageSaver::write_worker_loop_(unsigned long worker_i) noexcept
+  {
+    QueueCond* const queue_cond = worker_conds_[worker_i].get();
+    std::deque<QueuedOperation> operations;
+
+    while(true)
+    {
+      {
+        std::unique_lock<std::mutex> lock(queue_cond->mutex);
+        queue_cond->cond.wait(
+          lock,
+          [this, queue_cond]()
+          {
+            return !write_workers_active_object_->active() ||
+              queue_cond->queued_operations.load(std::memory_order_acquire) > 0;
+          });
+      }
+
+      unsigned long processed_operations = 0;
+
+      for (std::size_t chunk_i = worker_i;
+        chunk_i < files_.size(); chunk_i += write_workers_count_)
+      {
+        FileHolder& file_holder = *files_[chunk_i];
+        {
+          std::lock_guard<std::mutex> guard(file_holder.queue_lock);
+          if (file_holder.queue.empty())
+          {
+            continue;
+          }
+
+          operations.swap(file_holder.queue);
+        }
+
+        const unsigned long operations_count = operations.size();
+        processed_operations += operations_count;
+        queue_cond->queued_operations.fetch_sub(
+          operations_count,
+          std::memory_order_acq_rel);
+
+        while (!operations.empty())
+        {
+          const QueuedOperation& operation = operations.front();
+          try
+          {
+            process_queued_operation_(operation);
+          }
+          catch(const eh::Exception& ex)
+          {
+            set_background_error_(ex.what());
+            logger_->sstream(
+              Logging::Logger::EMERGENCY,
+              Aspect::MESSAGE_SAVER,
+              "ADS-IMPL-?") << "MessageSaver::write_worker_loop_(): "
+              "can't write operation: " << ex.what();
+          }
+          catch(...)
+          {
+            set_background_error_("unknown write operation error");
+            logger_->sstream(
+              Logging::Logger::EMERGENCY,
+              Aspect::MESSAGE_SAVER,
+              "ADS-IMPL-?") << "MessageSaver::write_worker_loop_(): "
+              "can't write operation: unknown error";
+          }
+
+          operations.pop_front();
+        }
+
+        if (pending_operations_.fetch_sub(
+             operations_count,
+             std::memory_order_acq_rel) == operations_count)
+        {
+          std::lock_guard<std::mutex> lock(pending_cond_lock_);
+          pending_cond_.notify_all();
+        }
+      }
+
+      if (!write_workers_active_object_->active() &&
+        processed_operations == 0 &&
+        queue_cond->queued_operations.load(std::memory_order_acquire) == 0)
+      {
+        break;
+      }
+    }
+  }
+
+  void
+  MessageSaver::process_queued_operation_(const QueuedOperation& operation)
+    /*throw(eh::Exception)*/
+  {
+    FileHolderGuard_var file_holder_guard = get_file_holder_i_(operation.chunk_i);
+    file_holder_guard->write(&operation.op_index, sizeof(operation.op_index));
+    file_holder_guard->write(operation.membuf);
+  }
+
+  void
+  MessageSaver::wait_pending_operations_() const noexcept
+  {
+    if (pending_operations_.load(std::memory_order_acquire) == 0)
+    {
+      return;
+    }
+
+    std::unique_lock<std::mutex> lock(pending_cond_lock_);
+    pending_cond_.wait(
+      lock,
+      [this]()
+      {
+        return pending_operations_.load(std::memory_order_acquire) == 0;
+      });
+  }
+
+  void
+  MessageSaver::set_background_error_(const char* error) noexcept
+  {
+    try
+    {
+      if (background_error_set_.load(std::memory_order_acquire))
+      {
+        return;
+      }
+
+      std::lock_guard<std::mutex> lock(background_error_lock_);
+      if (background_error_.empty())
+      {
+        background_error_ = error;
+        background_error_set_.store(true, std::memory_order_release);
+      }
+    }
+    catch(...)
+    {}
+  }
+
+  void
+  MessageSaver::check_background_error_() const
+  {
+    if (!background_error_set_.load(std::memory_order_acquire))
+    {
+      return;
+    }
+
+    std::lock_guard<std::mutex> lock(background_error_lock_);
+    Stream::Error ostr;
+    ostr << "MessageSaver background error: " << background_error_;
+    throw Exception(ostr);
   }
 }
 }
--- ProfilingCommons/MessageSaver.hpp
+++ ProfilingCommons/MessageSaver.hpp
@@ -1,8 +1,14 @@
 #pragma once
 
+#include <atomic>
+#include <condition_variable>
+#include <cstdint>
+#include <deque>
 #include <list>
 #include <memory>
+#include <mutex>
 #include <string>
+#include <thread>
 #include <vector>
 
 #include <Generics/CompositeActiveObject.hpp>
@@ -11,13 +17,10 @@
 #include <Logger/Logger.hpp>
 #include <ReferenceCounting/AtomicImpl.hpp>
 #include <ReferenceCounting/SmartPtr.hpp>
-#include <Sync/SyncPolicy.hpp>
 
 #include <ProfilingCommons/FileWriter.hpp>
 
-namespace AdServer
-{
-namespace ProfilingCommons
+namespace AdServer::ProfilingCommons
 {
   /*
    * MessageSaver
@@ -31,7 +34,7 @@ namespace ProfilingCommons
   public:
     DECLARE_EXCEPTION(Exception, eh::DescriptiveException);
 
-    typedef std::list<std::string> FileNameList;
+    using FileNameList = std::list<std::string>;
 
   public:
     MessageSaver(
@@ -39,7 +42,8 @@ namespace ProfilingCommons
       const char* output_dir,
       const char* file_prefix,
       unsigned long chunks_count,
-      const Generics::Time& flush_period);
+      const Generics::Time& flush_period,
+      unsigned long threads_count);
 
     void
     flush(FileNameList* dumped_files = 0)
@@ -47,9 +51,7 @@ namespace ProfilingCommons
 
     // can be used only for files with single operation
     void
-    write(
-      std::size_t distrib_hash,
-      const Generics::MemBuf& membuf)
+    write(std::size_t distrib_hash, const Generics::MemBuf& membuf)
       /*throw(eh::Exception)*/;
 
     // save operation in format:
@@ -60,15 +62,42 @@ namespace ProfilingCommons
     write_operation(
       std::size_t distrib_hash,
       unsigned long op_index,
-      const Generics::MemBuf& membuf)
+      Generics::MemBuf&& membuf)
       /*throw(eh::Exception)*/;
 
   protected:
-    typedef Sync::Policy::PosixThread SyncPolicy;
+    struct QueueCond
+    {
+      std::mutex mutex;
+      std::condition_variable cond;
+      std::atomic<unsigned long> queued_operations{0};
+    };
+
+    using QueueCondPtr = std::shared_ptr<QueueCond>;
+
+    struct QueuedOperation
+    {
+      QueuedOperation(
+        unsigned long chunk_i_val,
+        std::uint32_t op_index_val,
+        Generics::MemBuf&& membuf_val) noexcept;
+
+      QueuedOperation(const QueuedOperation&) = delete;
+      QueuedOperation& operator=(const QueuedOperation&) = delete;
+      QueuedOperation(QueuedOperation&&) noexcept = default;
+      QueuedOperation& operator=(QueuedOperation&&) = delete;
+
+      unsigned long chunk_i;
+      std::uint32_t op_index;
+      Generics::MemBuf membuf;
+    };
 
     struct FileHolder: public ReferenceCounting::AtomicImpl
     {
-      mutable SyncPolicy::Mutex lock;
+      std::mutex lock;
+      std::mutex queue_lock;
+      std::deque<QueuedOperation> queue;
+      QueueCondPtr queue_cond;
       std::string file_name;
       std::string tmp_file_name;
       std::unique_ptr<AdServer::ProfilingCommons::FileWriter> file_writer;
@@ -77,8 +106,7 @@ namespace ProfilingCommons
       virtual ~FileHolder() noexcept {}
     };
 
-    typedef ReferenceCounting::QualPtr<FileHolder>
-      FileHolder_var;
+    using FileHolder_var = ReferenceCounting::QualPtr<FileHolder>;
 
     class FileHolderGuard: public ReferenceCounting::DefaultImpl<>
     {
@@ -94,51 +122,88 @@ namespace ProfilingCommons
         /*throw(eh::Exception)*/;
 
     protected:
-      SyncPolicy::WriteGuard guard_;
       FileHolder_var file_holder_;
+      std::unique_lock<std::mutex> guard_;
     };
 
-    typedef ReferenceCounting::QualPtr<FileHolderGuard>
-      FileHolderGuard_var;
+    using FileHolderGuard_var = ReferenceCounting::QualPtr<FileHolderGuard>;
 
-    typedef std::vector<FileHolder_var> FileHolderArray;
+    using FileHolderArray = std::vector<FileHolder_var>;
+
+    class WriteWorkers;
+
+    using WriteWorkers_var = ReferenceCounting::SmartPtr<WriteWorkers>;
 
   protected:
     virtual
-    ~MessageSaver()
-      noexcept;
+    ~MessageSaver() noexcept;
 
     FileHolderGuard_var
     get_file_holder_(std::size_t hash)
       /*throw(eh::Exception)*/;
 
-    void
-    dump_file_holder_(
-      FileHolder& file_holder,
-      FileNameList* dumped_files)
+    FileHolderGuard_var
+    get_file_holder_i_(unsigned long chunk_i)
       /*throw(eh::Exception)*/;
 
     void
-    init_file_holder_i_(
-      FileHolder& file_holder, unsigned long chunk_i)
+    dump_file_holder_(FileHolder& file_holder, FileNameList* dumped_files)
+      /*throw(eh::Exception)*/;
+
+    void
+    init_file_holder_i_(FileHolder& file_holder, unsigned long chunk_i)
       /*throw(eh::Exception)*/;
 
     void
     flush_logs_() noexcept;
+
+    void
+    start_write_workers_();
+
+    void
+    notify_write_workers_();
+
+    void
+    wait_write_workers_();
+
+    void
+    write_worker_loop_(unsigned long worker_i) noexcept;
+
+    void
+    process_queued_operation_(const QueuedOperation& operation)
+      /*throw(eh::Exception)*/;
+
+    void
+    wait_pending_operations_() const noexcept;
+
+    void
+    set_background_error_(const char* error) noexcept;
+
+    void
+    check_background_error_() const;
 
   protected:
     Logging::Logger_var logger_;
     const std::string output_dir_;
     const std::string output_file_prefix_;
     const Generics::Time flush_period_;
+    const unsigned long write_workers_count_;
 
     Generics::ActiveObjectCallback_var callback_;
+    WriteWorkers_var write_workers_active_object_;
     Generics::TaskRunner_var task_runner_;
     Generics::Planner_var scheduler_;
     FileHolderArray files_;
+
+    std::atomic<unsigned long> pending_operations_{0};
+    mutable std::mutex pending_cond_lock_;
+    mutable std::condition_variable pending_cond_;
+    std::atomic<bool> background_error_set_{false};
+    mutable std::mutex background_error_lock_;
+    std::string background_error_;
+    std::vector<QueueCondPtr> worker_conds_;
+    std::vector<std::thread> write_workers_;
   };
 
-  typedef ReferenceCounting::SmartPtr<MessageSaver>
-    MessageSaver_var;
-}
+  using MessageSaver_var = ReferenceCounting::SmartPtr<MessageSaver>;
 }
